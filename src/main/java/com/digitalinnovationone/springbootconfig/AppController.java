ab850ca0ff83e18ac34083fb18b633b3ba42bf7e
Luís Felipe de Melo Nunes <luisfelipe5417@gmail.com>
2020-07-06 20:51:53 -0300

    Estudo de variáveis de ambiente

--- src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
+++ src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -7,11 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
+
     @Value("${app.message}")
     private String appMessage;
 
     @GetMapping("/")
     public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable() {
+        return "A seguinte varíavel de ambiente foi passada: " + dbEnvironmentVariable;
     }
 }
